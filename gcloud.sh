d796b232318383f1ded5693f32c1d90a12bb6dec
david Petrov <davidpetrovx@gmail.com>
2020-06-06 16:54:44 -0400

    Added flags, extra status case

--- gcloud.sh
+++ gcloud.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#Arg 1: [restart/stop/start]
-#Arg 2 : [Virtual Machine Name (Ex: windows-machine)]
+# Arg 1: [restart -r/ end -e /start -s /status -p]
+# Arg 2 : [gcloud instance name (Ex: windows-machine)
 
 if ! [ "$#" = 2 ]
 then
@@ -8,45 +8,48 @@ then
     exit 2
 fi
 
-gcloud compute instances list | grep "$2"
-if ! [ "$?" = 0 ]
-then
-    echo "Instance $2 does not exist"
-    exit 1
-fi
+echo "Checking if instance $2 exists..."
+(gcloud compute instances list | grep "$2" > /dev/null) && echo "Failure: instance $2 does does not exist" | exit 0
+echo "Success: Instance $2 exists. Continuing..."
 
-if [ "$1" = "start" ]
-then
-    echo "Starting cloud instances: $2 ..."
-    gcloud compute instances start "$2"
-    if [ "$?" = 1 ]
-    then
-        echo "Exit: Could not start instance"
-        exit 1
-    fi
-elif [ "$1" = "restart" ]
-then
-    echo "Restarting cloud instances: $2 ..."
-    gcloud compute instances stop "$2"
-    if [ "$?" = 0 ]
-    then
+case "$1" in
+    "-p" | "status")
+        gcloud compute instances list --filter=name:"$2" --format="table[box,title='$2 Status'](id,status)"
+        ;;
+    "-s" | "start")
+        echo "Starting cloud instances: $2 ..."
         gcloud compute instances start "$2"
-    else
-        echo "Exit: Could not stop instance"
-        exit 1
-    fi
-elif [ "$1" = "stop" ]
-then
-    echo "Stopping cloud instances: $2 ..."
-    gcloud compute instances stop "$2"
-    if [ "$?" = 1 ]
-    then
-        echo "Exit: Could not stop instance"
-        exit 1
-    fi
-else
-    echo "Exit: Invalid argument"
-    exit 127
-fi
+        if [ "$?" = 1 ]
+        then
+            echo "Failure: Could not start instance"
+            exit 1
+        fi
+        ;;
+    "-r" | "restart")
+        echo "Restarting cloud instances: $2 ..."
+        gcloud compute instances stop "$2"
+        if [ "$?" = 0 ]
+        then
+            gcloud compute instances start "$2"
+        else
+            echo "Failure: Could not stop $2 instance. Aborting..."
+            exit 1
+        fi
+        ;;
+    "-e" | "end")
+        echo "Stopping cloud instance: $2 ..."
+        gcloud compute instances stop "$2"
+        if [ "$?" = 1 ]
+        then
+            echo "Failure: Could not stop $2 instance. Aborting..."
+
+            exit 1
+        fi
+        ;;
+    *)
+        echo "Exit: Invalid argument"
+        exit 127
+        ;;
+esac
 
 exit 0
